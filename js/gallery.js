e63d71c36c9c7867995d16a00259d77e27c2316e
Chan-Yu-Ming <92195865+Chan-Yu-Ming@users.noreply.github.com>
2025-11-23 12:49:45 +0800

    Add image preview and reset functions

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,13 @@
+function upDate(previewPic){
+let x=document.getElementById('image');
+x.style.backgroundImage = "url("+previewPic.src+")";
+x.innerHTML=previewPic.alt;
+x.style.backgroundColor = "#CCEECC";
+}
 
+function unDo(){
+let y = document.getElementById('image');
+y.style.backgroundImage = "url()";
+y.innerHTML="Hover over an image below to display here.";
+y.style.backgroundColor = "#8e68ff";
+}
